9f0a47e30ecdf15ed4d63deff39b05b07dcec680
Grigory <sobenin.g@gmail.com>
2019-06-11 23:36:46 +0300

    Rename

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ import Board from './Board';
 
 let result: Board;
 
-function backtrack(values: number[], possibleValues: number[]): number {
+function backtjump(values: number[], possibleValues: number[]): number {
     if (possibleValues.length === 0) {
         result = new Board(values);
         result.print();
@@ -12,7 +12,7 @@ function backtrack(values: number[], possibleValues: number[]): number {
     let error;
 
     for (let i = 0; i < possibleValues.length; i++) {
-        error = backtrack(values.concat([possibleValues[i]]), possibleValues.filter((pValue, j) => i !== j));
+        error = backtjump(values.concat([possibleValues[i]]), possibleValues.filter((pValue, j) => i !== j));
         if (error < values.length) {
             return error;
         }
@@ -23,4 +23,4 @@ function backtrack(values: number[], possibleValues: number[]): number {
     }
 }
 
-backtrack([], [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+backtjump([], [1, 2, 3, 4, 5, 6, 7, 8, 9]);
